e935d66a5342cfc008de4dab577142a350e90f15
Mikhail.Chernichenko <mchernichenko@mail.ru>
2022-03-03 00:43:40 +0300

    camel-intro

--- exercises/camel-intro/src/main/java/exercise/App.java
+++ exercises/camel-intro/src/main/java/exercise/App.java
@@ -5,9 +5,17 @@ import org.apache.camel.impl.DefaultCamelContext;
 
 class App {
     public static void main(String[] args) throws Exception {
-        CamelContext camelContext = new DefaultCamelContext();
 
-        camelContext.addRoutes(new HelloWorldRoute());
+        // create a CamelContext
+        CamelContext camel = new DefaultCamelContext();
+
+        // add routes
+        camel.addRoutes(new HelloWorldRoute());
+
+        // start is not blocking
+        //camel.start();
+
+        //camel.stop();
 
     }
 }
